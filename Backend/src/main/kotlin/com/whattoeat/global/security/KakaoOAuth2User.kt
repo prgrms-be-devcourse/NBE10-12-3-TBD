b501c9d81cb59c69ac9b00a4ca89a12259b71f72
SOL4R1S <mingh662@gmail.com>
2026-07-30 11:15:30 +0900

    fix: KakaoOAuth2User.kt null 허용 문제 해결(#4)

--- Backend/src/main/kotlin/com/whattoeat/global/security/KakaoOAuth2User.kt
+++ Backend/src/main/kotlin/com/whattoeat/global/security/KakaoOAuth2User.kt
@@ -7,15 +7,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 
 class KakaoOAuth2User(val user: User) : OAuth2User {
 
-    override fun getAttributes(): Map<String, Any> {
-        return emptyMap()
-    }
+    override fun getAttributes(): Map<String, Any> = emptyMap()
 
-    override fun getAuthorities(): Collection<GrantedAuthority> {
-        return listOf(SimpleGrantedAuthority("ROLE_${user.role.name}"))
-    }
+    override fun getAuthorities(): Collection<GrantedAuthority> = listOf(SimpleGrantedAuthority("ROLE_${user.role.name}"))
 
-    override fun getName(): String {
-        return user.id.toString()
-    }
+    override fun getName(): String = checkNotNull(user.id){"OAuth2 유저 ID가 없습니다."}.toString()
 }
