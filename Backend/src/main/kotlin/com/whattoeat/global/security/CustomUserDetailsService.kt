c3fbe725b96815b2cd2743179e3b7f4ec78c9427
SOL4R1S <mingh662@gmail.com>
2026-07-30 11:06:35 +0900

    fix: CustomUserDetailsService.kt userId를 Long으로 파싱할 때의 문제 해결(#4)

--- Backend/src/main/kotlin/com/whattoeat/global/security/CustomUserDetailsService.kt
+++ Backend/src/main/kotlin/com/whattoeat/global/security/CustomUserDetailsService.kt
@@ -10,10 +10,14 @@ import org.springframework.stereotype.Service
 class CustomUserDetailsService(private val userRepository: UserRepository) : UserDetailsService {
 
     override fun loadUserByUsername(userId: String): UserDetails {
-        val user = userRepository.findById(userId.toLong())
-            .orElseThrow{
+        val parsedUserId = userId.toLongOrNull()
+            ?: throw UsernameNotFoundException("유저를 찾을 수 없습니다.")
+
+        val user = userRepository.findById(parsedUserId)
+            .orElseThrow {
                 UsernameNotFoundException("유저를 찾을 수 없습니다.")
             }
+
         return CustomUserDetails(user)
     }
 }
