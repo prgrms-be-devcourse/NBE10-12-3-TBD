bbbb9bc3244b44e8e2b72cda70c773266deb58a4
SOL4R1S <mingh662@gmail.com>
2026-07-30 12:24:02 +0900

    refactor: Service Java->kt 마이그레이션(#7)

---
+++ Backend/src/main/kotlin/com/whattoeat/domain/comment/service/CommentService.kt
@@ -0,0 +1,48 @@
+package com.whattoeat.domain.comment.service
+
+import com.whattoeat.domain.comment.dto.CommentRequest
+import com.whattoeat.domain.comment.dto.CommentResponse
+import com.whattoeat.domain.comment.dto.CommentResponse.Companion.from
+import com.whattoeat.domain.comment.entity.Comment
+import com.whattoeat.domain.comment.repository.CommentRepository
+import com.whattoeat.domain.feed.repository.FeedRepository
+import com.whattoeat.domain.user.repository.UserRepository
+import com.whattoeat.global.exception.CommentNotFoundException
+import com.whattoeat.global.exception.FeedNotFoundException
+import com.whattoeat.global.exception.UserNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class CommentService(
+    private val commentRepository: CommentRepository,
+    private val feedRepository: FeedRepository,
+    private val userRepository: UserRepository
+) {
+    fun getComments(feedId: Long): List<CommentResponse> =
+        commentRepository.findByFeedId(feedId).map(CommentResponse::from)
+
+
+    @Transactional
+    fun createComment(feedId: Long, userId: Long, request: CommentRequest): CommentResponse {
+        val feed = feedRepository.findById(feedId)
+            .orElseThrow { FeedNotFoundException(feedId) }
+        val user = userRepository.findById(userId)
+            .orElseThrow { UserNotFoundException(userId) }
+        val comment = Comment(
+            feed = feed,
+            user = user,
+            content = requireNotNull(request.content){"댓글 내용이 없습니다."}
+        )
+        return from(commentRepository.save(comment))
+    }
+
+    @Transactional
+    fun deleteComment(feedId: Long, commentId: Long) {
+        val comment = commentRepository.findById(commentId)
+            .orElseThrow { CommentNotFoundException(commentId) }
+        require(comment.feed.id == feedId) { "해당 피드의 댓글이 아닙니다." }
+        commentRepository.delete(comment)
+    }
+}
