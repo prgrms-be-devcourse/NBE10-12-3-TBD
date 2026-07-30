28f305c67922cb38ca39e226e4217e613c3d704f
SOL4R1S <mingh662@gmail.com>
2026-07-31 02:31:56 +0900

    chore: Comment.kt protected set 적용 (#7)

--- Backend/src/main/kotlin/com/whattoeat/domain/comment/entity/Comment.kt
+++ Backend/src/main/kotlin/com/whattoeat/domain/comment/entity/Comment.kt
@@ -7,15 +7,22 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "feed_comment")
-class Comment (
+class Comment(
+    feed: Feed,
+    user: User,
+    content: String,
+) : BaseEntity() {
     @field:JoinColumn(name = "feed_id", nullable = false)
     @field:ManyToOne(fetch = FetchType.LAZY)
-    var feed: Feed,
+    var feed: Feed = feed
+        protected set
 
     @field:JoinColumn(name = "user_id", nullable = false)
     @field:ManyToOne(fetch = FetchType.LAZY)
-    var user: User,
+    var user: User = user
+        protected set
 
     @field:Column(nullable = false, length = 500)
-    var content: String
-) : BaseEntity()
+    var content: String = content
+        protected set
+}
