bb7fa1cb6a7b5351bd201af97367613a832bc33e
SOL4R1S <mingh662@gmail.com>
2026-07-30 12:23:30 +0900

    refactor: Repository Java->kt 마이그레이션(#7)

---
+++ Backend/src/main/kotlin/com/whattoeat/domain/comment/repository/CommentRepository.kt
@@ -0,0 +1,16 @@
+package com.whattoeat.domain.comment.repository
+
+import com.whattoeat.domain.comment.entity.Comment
+import org.springframework.data.repository.query.Param
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface CommentRepository : JpaRepository<Comment, Long> {
+    fun findByFeedId(feedId: Long): List<Comment>
+
+    // FeedListResponse에 댓글 수 넣기 위한 카운트 쿼리 추가
+    fun countByFeedId(feedId: Long): Long
+
+    @Query("SELECT c.feed.id, COUNT(c) FROM Comment c WHERE c.feed.id IN :feedIds GROUP BY c.feed.id")
+    fun countByFeedIds(@Param("feedIds") feedIds: List<Long>): List<Array<Any>>
+}
